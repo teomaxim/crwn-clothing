8d11adcf707a011b0cee460548bfac4bd424c61a
Nguyen Phuong Nam <phuongnam.nguyen@ogilvy.com>
2020-04-22 09:38:23 +0700

    add colleciton item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/hompage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
