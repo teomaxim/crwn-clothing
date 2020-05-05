56d81f1c22ce55ee6c7c77050120cec14eb4896d
Nguyen Phuong Nam <phuongnam.nguyen@ogilvy.com>
2020-05-05 11:46:05 +0700

    create item cart and cart dropdown with cartitem reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './custom-button.style.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => {
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => {
     return <button
-        className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`}
+        className={`${inverted ? 'inverted' : '' } custom-button`}
         {...otherProps}>
         {console.log(isGoogleSignIn)}
         {children}
